f523f77001a0488834eeed945634bbc9d89d1591
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-03-28 16:57:43 +0700

    remove repo base package

--- backend_laptop_store/laptopstore/src/main/java/chivi/laptopstore/LaptopstoreApplication.java
+++ backend_laptop_store/laptopstore/src/main/java/chivi/laptopstore/LaptopstoreApplication.java
@@ -3,9 +3,7 @@ package chivi.laptopstore;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@EnableJpaRepositories(basePackages = "chivi.laptopstore.repositories.entities")
 @EnableJpaAuditing
 @SpringBootApplication
 public class LaptopstoreApplication {

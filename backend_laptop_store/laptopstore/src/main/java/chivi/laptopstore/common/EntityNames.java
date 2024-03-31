fc6de2805e58be20be8d9b8bbb5beab31b04613a
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-03-31 12:15:03 +0700

    add join table name, join column name, remove column category and image

--- backend_laptop_store/laptopstore/src/main/java/chivi/laptopstore/common/EntityNames.java
+++ backend_laptop_store/laptopstore/src/main/java/chivi/laptopstore/common/EntityNames.java
@@ -7,18 +7,17 @@ public class EntityNames {
     public static final String TABLE_CATEGORY = "category";
     public static final String TABLE_PRODUCT = "product";
     public static final String TABLE_IMAGE = "image";
+    public static final String TABLE_VERIFICATION_TOKEN = "verification_token";
+    public static final String TABLE_PRODUCT_CATEGORY = "product_category";
+    public static final String TABLE_PRODUCT_IMAGE = "product_image";
 
     // Column name
     public static final String COLUMN_ACCOUNT_ID = "account_id";
     public static final String COLUMN_ACCOUNT_FULL_NAME = "full_name";
     public static final String COLUMN_ACCOUNT_ROLE = "role";
-    public static final String TABLE_VERIFICATION_TOKEN = "verification_token";
 
     public static final String COLUMN_BRAND_NAME = "brand_name";
     public static final String COLUMN_BRAND_SLUG = "brand_slug";
-
-    public static final String COLUMN_CATEGORY_NAME = "category_name";
-    public static final String COLUMN_CATEGORY_PATH = "category_path";
 
     public static final String COLUMN_PRODUCT_NAME = "product_name";
     public static final String COLUMN_PRODUCT_SLUG = "product_slug";
@@ -30,10 +29,12 @@ public class EntityNames {
     public static final String COLUMN_REVIEW_COUNT = "review_count";
     public static final String COLUMN_LIKE_COUNT = "like_count";
 
-
-    public static final String COLUMN_IMAGE_ID = "image_id";
     public static final String COLUMN_IMAGE_PUBLIC_ID = "public_id";
     public static final String COLUMN_IMAGE_SECURE_URL = "secure_url";
+
+    public static final String JOIN_COLUMN_PRODUCT_ID = "product_id";
+    public static final String JOIN_COLUMN_CATEGORY_ID = "category_id";
+    public static final String JOIN_COLUMN_IMAGE_ID = "image_id";
 
     public static final String COLUMN_CREATED_AT = "created_at";
     public static final String COLUMN_UPDATED_AT = "updated_at";

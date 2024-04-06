9fa7046317a76195132d1418d3934ac8915a64fb
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-04-06 16:33:51 +0700

    define function decode jwt by token

--- frontend_admin/laptop_store_admin_page/src/utils/token.ts
+++ frontend_admin/laptop_store_admin_page/src/utils/token.ts
@@ -1,6 +1,18 @@
+import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
 import { EKeys } from '~/common/enums';
 
+export type jwtType = {
+    sub: string;
+    iat: number;
+    // "iss": "laptopstore",
+    iss: string;
+    exp: number;
+};
+
 export default function getSessionToken() {
     return `Bearer ${cookies().get(EKeys.TOKEN)?.value}`;
+}
+export function decodeJwt<payload = jwt.JwtPayload>(token: string) {
+    return jwt.decode(token) as payload;
 }

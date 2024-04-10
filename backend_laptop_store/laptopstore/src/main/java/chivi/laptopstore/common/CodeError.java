43c24615ea5c710e0b38280f904739e7d43cd0a4
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-04-10 22:21:27 +0700

    response error body object

---
+++ backend_laptop_store/laptopstore/src/main/java/chivi/laptopstore/common/CodeError.java
@@ -0,0 +1,9 @@
+package chivi.laptopstore.common;
+
+public class CodeError {
+    public static final String CONFLICT_ERROR = "ConflictError";
+    public static final String TOKEN_EXPIRED_ERROR = "TokenExpiredError";
+    public static final String TOKEN_INVALID_ERROR = "TokenInvalidError";
+    public static final String TOKEN_SIGNATURE_ERROR = "TokenSignatureError";
+    public static final String USERNAME_OR_PASSWORD_INCORRECT_ERROR = "UsernameOrPasswordInCorrectError";
+}
--- backend_laptop_store/laptopstore/src/main/java/chivi/laptopstore/exception/handler/GlobalHandler.java
+++ backend_laptop_store/laptopstore/src/main/java/chivi/laptopstore/exception/handler/GlobalHandler.java
@@ -1,7 +1,12 @@
 package chivi.laptopstore.exception.handler;
 
+import chivi.laptopstore.common.CodeError;
+import chivi.laptopstore.communication.error.ErrorBodyResponse;
+import chivi.laptopstore.communication.error.FamilyErrorBodyResponse;
+import chivi.laptopstore.communication.error.FieldErrorBodyResponse;
+import chivi.laptopstore.communication.error.NameErrorBodyResponse;
 import chivi.laptopstore.exception.BaseException;
-import chivi.laptopstore.models.responses.ErrorResponse;
+import io.jsonwebtoken.ExpiredJwtException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,44 +18,75 @@ import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.ServletWebRequest;
 
 import java.util.Map;
 
 @Slf4j
 @RestControllerAdvice
 public class GlobalHandler {
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public NameErrorBodyResponse handleInternalError(Exception exception, ServletWebRequest request) {
+        String className = exception.getClass().getName();
+        String message = exception.getMessage();
+        String requestUri = request.getRequest().getRequestURI();
+        return new NameErrorBodyResponse(className, message, requestUri);
+    }
+
     @ExceptionHandler(BaseException.class)
-    public ResponseEntity<?> commonHandler(BaseException exception) {
-        return ResponseEntity.status(exception.getCode()).body(Map.of("message", exception.getMessage()));
+    public ResponseEntity<ErrorBodyResponse> handleBaseException(BaseException exception, ServletWebRequest request) {
+        int codeStatus = exception.getCode();
+        String message = exception.getMessage();
+        String requestUri = request.getRequest().getRequestURI();
+        ErrorBodyResponse body = new ErrorBodyResponse(message, requestUri);
+        return ResponseEntity.status(codeStatus).body(body);
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ErrorResponse handleMethodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        ErrorResponse errorResponse = new ErrorResponse("Field invalid");
+    public FieldErrorBodyResponse handleArgumentNotValid(MethodArgumentNotValidException exception, ServletWebRequest request) {
+        String message = exception.getMessage();
+        String requestUri = request.getRequest().getRequestURI();
+        FieldErrorBodyResponse bodyResponse = new FieldErrorBodyResponse(message, requestUri);
+
         exception.getBindingResult().getAllErrors().forEach(error -> {
             String fieldName = ((FieldError) error).getField();
             String defaultMessage = error.getDefaultMessage();
-            errorResponse.addFieldItem(fieldName, defaultMessage);
+            bodyResponse.addErrorItem(fieldName, defaultMessage);
         });
-        return errorResponse;
+        return bodyResponse;
+    }
+
+    @ExceptionHandler(ExpiredJwtException.class)
+    @ResponseStatus(HttpStatus.UNAUTHORIZED)
+    public FamilyErrorBodyResponse handleExpiredJwt(ExpiredJwtException exception, ServletWebRequest request) {
+        String message = exception.getMessage();
+        String requestUri = request.getRequest().getRequestURI();
+        return new FamilyErrorBodyResponse(CodeError.TOKEN_EXPIRED_ERROR, message, requestUri);
     }
 
     @ExceptionHandler(AuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public Map<String, String> authenticationHandler(AuthenticationException exception) {
-        return Map.of("classException", exception.getClass().getSimpleName(), "message", exception.getMessage());
+    public NameErrorBodyResponse handleAuthentication(AuthenticationException exception, ServletWebRequest request) {
+        String className = exception.getClass().getName();
+        String message = exception.getMessage();
+        String requestUri = request.getRequest().getRequestURI();
+        return new NameErrorBodyResponse(className, message, requestUri);
     }
 
     @ExceptionHandler(InsufficientAuthenticationException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public Map<String, String> handleInsufficientAuthenticationException(InsufficientAuthenticationException exception) {
-        return Map.of("description", "credentials are missing.", "message", exception.getMessage());
+    public Map<String, String> handleInsufficientAuthentication(InsufficientAuthenticationException exception, ServletWebRequest request) {
+        String requestUri = request.getRequest().getRequestURI();
+        return Map.of("description", "Credentials are missing.", "message", exception.getMessage());
     }
 
     @ExceptionHandler(BadCredentialsException.class)
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
-    public Map<String, String> handleBadCredentialsException(BadCredentialsException exception) {
-        return Map.of("description", "username or password is incorrect.", "message", exception.getMessage());
+    public FamilyErrorBodyResponse handleBadCredentials(BadCredentialsException exception, ServletWebRequest request) {
+        String message = exception.getMessage();
+        String requestUri = request.getRequest().getRequestURI();
+        return new FamilyErrorBodyResponse(CodeError.USERNAME_OR_PASSWORD_INCORRECT_ERROR, message, requestUri);
     }
 }

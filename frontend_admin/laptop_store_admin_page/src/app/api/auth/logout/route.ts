c119b77047a4339086ae5fe977ad6f67b0302ca4
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-04-10 21:40:19 +0700

    clear cookies access and refresh token

--- frontend_admin/laptop_store_admin_page/src/app/api/auth/logout/route.ts
+++ frontend_admin/laptop_store_admin_page/src/app/api/auth/logout/route.ts
@@ -1,7 +1,15 @@
 import { cookies } from 'next/headers';
 import { EKeys, EPath } from '~/common/enums';
+import httpRequest from '~/libs/http.request';
 
 export async function GET() {
-    cookies().delete(EKeys.TOKEN);
+    const cookieStore = cookies();
+    const refreshToken = cookieStore.get(EKeys.REFRESH_TOKEN)?.value ?? '';
+
+    await httpRequest.delete('auth/logout', { params: { token: refreshToken } });
+
+    cookieStore.delete(EKeys.ACCESS_TOKEN);
+    cookieStore.delete(EKeys.REFRESH_TOKEN);
+
     return Response.json({ message: 'logout!!', path: EPath.AUTH_LOGIN });
 }

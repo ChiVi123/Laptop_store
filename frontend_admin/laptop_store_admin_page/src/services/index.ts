9d1751ed9dcbec3c14dd6f0081eae32be8d70ce3
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-04-21 18:04:04 +0700

    remove export services, ignore auth service

--- frontend_admin/laptop_store_admin_page/src/services/index.ts
+++ frontend_admin/laptop_store_admin_page/src/services/index.ts
@@ -1,6 +1,1 @@
 export * as authService from './auth';
-export * as categoryService from './category';
-export * as findAllService from './find.all';
-export * as findOneService from './find.one';
-export * as productService from './product';
-export * as uploadFileService from './upload.file';

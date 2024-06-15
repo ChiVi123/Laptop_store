eae26dcf38bc77be55b1e32cb2b78d219c5fc2cd
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-06-16 01:34:31 +0700

    add link navigate cart page in bottom navigation

--- frontend_client/laptop_store_client_page/src/components/bottom.navigation.tsx
+++ frontend_client/laptop_store_client_page/src/components/bottom.navigation.tsx
@@ -12,7 +12,7 @@ function BottomNavigation() {
     const handleRouteBack = () => router.back();
     const handleRouteForward = () => router.forward();
     return (
-        <div className='sticky right-0 bottom-0 left-0 flex lg:hidden justify-between p-2 bg-white border shadow-sm'>
+        <div className='fixed right-0 bottom-0 left-0 flex lg:hidden justify-between p-2 bg-white border shadow-sm'>
             <Button
                 type='button'
                 aria-label='btn-nav-prev'
@@ -24,20 +24,27 @@ function BottomNavigation() {
             >
                 <CaretLeftIcon className='w-6 h-6' />
             </Button>
+
             <Button type='button' aria-label='btn-nav-home' variant='ghost' size='icon' className='w-10 h-10' asChild>
                 <Link href='/'>
                     <HomeIcon className='w-6 h-6' />
                 </Link>
             </Button>
-            <Button type='button' aria-label='btn-nav-cate' variant='ghost' size='icon' className='w-10 h-10'>
-                <TokensIcon className='w-6 h-6' />
+
+            <Button type='button' aria-label='btn-nav-cate' variant='ghost' size='icon' className='w-10 h-10' asChild>
+                <Link href='/cart'>
+                    <TokensIcon className='w-6 h-6' />
+                </Link>
             </Button>
+
             <Button type='button' aria-label='btn-nav-cart' variant='ghost' size='icon' className='w-10 h-10'>
                 <CartIcon className='w-6 h-6' />
             </Button>
+
             <Button type='button' aria-label='btn-nav-acc' variant='ghost' size='icon' className='w-10 h-10'>
                 <PersonIcon className='w-6 h-6' />
             </Button>
+
             <Button
                 type='button'
                 aria-label='btn-nav-next'

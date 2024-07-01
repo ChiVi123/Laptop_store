bff5fb9d0291160c4dad301b1944fec411b744fe
ChiVi123 <nguyenhoangchivi@gmail.com>
2024-07-01 23:32:49 +0700

    delete file handle get and set localstorage

--- frontend_client/laptop_store_client_page/src/libs/utilities/storage.ts
+++
@@ -1,6 +0,0 @@
-export const storage = {
-    set: (key: string, value: any) => {
-        localStorage.setItem(key, JSON.stringify(value));
-    },
-    get: <T>(key: string, fallback: string): T => JSON.parse(localStorage.getItem(key) ?? fallback),
-};
